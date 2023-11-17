06c5fa1efb2ad80b727c0adc1dcd0c70e0f9a000
dvidCR <dvidcr100@gmail.com>
2023-11-17 18:28:46 +0100

    Undecimo Commit

--- Java/Eclipse-Workspace/clase_13112023/src/clase_13112023/Ejercicio_13112023.java
+++ Java/Eclipse-Workspace/clase_13112023/src/clase_13112023/Ejercicio_13112023.java
@@ -12,8 +12,24 @@ public class Ejercicio_13112023 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int aux = 4;
+		int aux2 = 5;
+		for(int i = 0; i < 7 ; i ++) {
+			if(i < 4) {
+				aux--;
+				aux2++;
+			}else{
+				aux++;
+				aux2--;
+			}
+			for(int j = 0; j < 10; j++) {
+				if(j < aux || j > aux2) {
+					System.out.print(" ");
+				}else{
+					System.out.print("+");
+				}
+			}
+			System.out.println();
+		}
 	}
-
 }
